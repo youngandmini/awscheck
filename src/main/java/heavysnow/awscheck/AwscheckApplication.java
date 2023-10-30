83e964574e466df8e25e9780a6ee6e6de0f8ff28
youngmin <dudals9901@naver.com>
2023-10-30 15:02:59 +0900

    Initial commit

---
+++ src/main/java/heavysnow/awscheck/AwscheckApplication.java
@@ -0,0 +1,13 @@
+package heavysnow.awscheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwscheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwscheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/heavysnow/awscheck/AwscheckApplicationTests.java
@@ -0,0 +1,13 @@
+package heavysnow.awscheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwscheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
